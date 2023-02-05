cd73461b91a27a46c74a79b812fbc6006aa15cd7
Nimesh_Piyumanth <88945963+NimeshPiyumantha@users.noreply.github.com>
2023-02-05 07:51:36 +0530

    Update Back_End directory and add AbstractAnnotationConfigDispatcherServletInitializer implement methods

--- POS_Back_End/src/main/java/lk/ijse/spring/WebAppInitializer.java
+++ POS_Back_End/src/main/java/lk/ijse/spring/WebAppInitializer.java
@@ -7,4 +7,18 @@ import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatche
  * @since : 0.1.0
  **/
 public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[0];
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[0];
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[0];
+    }
 }
