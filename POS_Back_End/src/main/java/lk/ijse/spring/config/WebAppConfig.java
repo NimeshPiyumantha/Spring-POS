d201a11c1de39c149e3b04d452640550b90b486b
Nimesh_Piyumanth <88945963+NimeshPiyumantha@users.noreply.github.com>
2023-02-05 16:28:05 +0530

    Update Back_End directory and Refactor Customer Controller using service layer

--- POS_Back_End/src/main/java/lk/ijse/spring/config/WebAppConfig.java
+++ POS_Back_End/src/main/java/lk/ijse/spring/config/WebAppConfig.java
@@ -1,11 +1,6 @@
 package lk.ijse.spring.config;
 
 import lk.ijse.spring.advisor.AppWideExceptionHandler;
-import lk.ijse.spring.controller.CustomerController;
-import lk.ijse.spring.controller.ItemController;
-import lk.ijse.spring.controller.PlaceOrderController;
-import org.modelmapper.ModelMapper;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -16,11 +11,6 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
  **/
 @Configuration
 @EnableWebMvc
-@ComponentScan(basePackageClasses = {CustomerController.class, ItemController.class, PlaceOrderController.class, AppWideExceptionHandler.class})
+@ComponentScan(basePackageClasses = {AppWideExceptionHandler.class}, basePackages = "lk.ijse.spring.controller")
 public class WebAppConfig {
-    @Bean
-    public ModelMapper modelMapper(){
-        return new ModelMapper();
-    }
-
 }
--- POS_Back_End/src/main/java/lk/ijse/spring/config/WebRootConfig.java
+++ POS_Back_End/src/main/java/lk/ijse/spring/config/WebRootConfig.java
@@ -1,5 +1,8 @@
 package lk.ijse.spring.config;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
@@ -9,5 +12,10 @@ import org.springframework.context.annotation.Import;
  **/
 @Configuration
 @Import(JPAConfig.class)
+@ComponentScan(basePackages = "lk.ijse.spring.service")
 public class WebRootConfig {
+    @Bean
+    public ModelMapper modelMapper(){
+        return new ModelMapper();
+    }
 }
