8098deb4cf56043dc6ab6480d204c37bc719b20d
Nimesh_Piyumanth <88945963+NimeshPiyumantha@users.noreply.github.com>
2023-02-05 07:50:44 +0530

    Update Back_End directory and add pom.xml to dependencies

---
+++ POS_Back_End/src/main/java/lk/ijse/spring/WebAppInitializer.java
@@ -0,0 +1,10 @@
+package lk.ijse.spring;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+/**
+ * @author : Nimesh Piyumantha
+ * @since : 0.1.0
+ **/
+public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+}
